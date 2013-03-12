b8fcbb4efcc771d8264b2f9d5a722bc2a84e5331
piotrekxyz <piotrekxyz@tlen.pl>
2013-03-12 13:14:36 +0100

    Update potega_pm.cpp

--- Potega/potega_pm.cpp
+++ Potega/potega_pm.cpp
@@ -19,9 +19,9 @@ int main() {
 	int os_cyfra = ost_znak-'0';
 	int liczba = 10*prz_os_cyfra+os_cyfra;
 
-	if (liczba == 0 & dl ==1 ) cout << 1 << endl << endl;
-	else if (liczba % 4 == 0) cout << 6 << endl << endl;
-	else if (liczba % 4 == 1) cout << 2 << endl << endl;
-	else if (liczba % 4 == 2) cout << 4 << endl << endl;
-	else cout << 8 << endl << endl;
+	if (liczba == 0 & dl ==1 ) cout << 1 << endl;
+	else if (liczba % 4 == 0) cout << 6 << endl;
+	else if (liczba % 4 == 1) cout << 2 << endl;
+	else if (liczba % 4 == 2) cout << 4 << endl;
+	else cout << 8 << endl;
 }
